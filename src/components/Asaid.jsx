332a58c385bf8ec007cf10aa2b63977d6a16b857
Demion-L <padovds@ukr.net>
2024-06-12 22:53:58 +0300

    added styles for honeycomb tiles

--- src/components/Asaid.jsx
+++ src/components/Asaid.jsx
@@ -5,19 +5,66 @@ const Asaid = () => {
   return (
     <ul className='honeycomb'>
       <li className='honeycomb-cell'>
+        <img src='/tiles/1.jpg' alt='' className='honeycomb-cell_img' />
         <div className='honeycomb-cell_title'>Web Development 1</div>
+        <div className='honeycomb-cell_description'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+          accusantium necessitatibus dolorem est? Magni itaque fugiat magnam
+          ratione aliquam quibusdam iusto dolor rem, earum ipsa maiores
+          consequuntur eos eius. Adipisci!
+        </div>
       </li>
       <li className='honeycomb-cell'>
+        <img src='/tiles/2.jpg' alt='' className='honeycomb-cell_img' />
         <div className='honeycomb-cell_title'>Web Development 2</div>
+        <div className='honeycomb-cell_description'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+          accusantium necessitatibus dolorem est? Magni itaque fugiat magnam
+          ratione aliquam quibusdam iusto dolor rem, earum ipsa maiores
+          consequuntur eos eius. Adipisci!
+        </div>
       </li>
       <li className='honeycomb-cell'>
+        <img src='/tiles/3.jpg' alt='' className='honeycomb-cell_img' />
         <div className='honeycomb-cell_title'>Web Development 3</div>
+        <div className='honeycomb-cell_description'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+          accusantium necessitatibus dolorem est? Magni itaque fugiat magnam
+          ratione aliquam quibusdam iusto dolor rem, earum ipsa maiores
+          consequuntur eos eius. Adipisci!
+        </div>
       </li>
       <li className='honeycomb-cell'>
+        <img src='/tiles/4.jpg' alt='' className='honeycomb-cell_img' />
         <div className='honeycomb-cell_title'>Web Development 4</div>
+        <div className='honeycomb-cell_description'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+          accusantium necessitatibus dolorem est? Magni itaque fugiat magnam
+          ratione aliquam quibusdam iusto dolor rem, earum ipsa maiores
+          consequuntur eos eius. Adipisci!
+        </div>
       </li>
       <li className='honeycomb-cell'>
+        <img src='/tiles/5.jpg' alt='' className='honeycomb-cell_img' />
         <div className='honeycomb-cell_title'>Web Development 5</div>
+        <div className='honeycomb-cell_description'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+          accusantium necessitatibus dolorem est? Magni itaque fugiat magnam
+          ratione aliquam quibusdam iusto dolor rem, earum ipsa maiores
+          consequuntur eos eius. Adipisci!
+        </div>
+      </li>
+      <li className='honeycomb-cell'>
+        <img src='/tiles/6.jpg' alt='' className='honeycomb-cell_img' />
+        <div className='honeycomb-cell_title'>Web Development 6</div>
+      </li>
+      <li className='honeycomb-cell'>
+        <img src='/tiles/7.jpg' alt='' className='honeycomb-cell_img' />
+        <div className='honeycomb-cell_title'>Web Development 7</div>
+      </li>
+      <li className='honeycomb-cell'>
+        <img src='/tiles/8.jpg' alt='' className='honeycomb-cell_img' />
+        <div className='honeycomb-cell_title'>Web Development 8</div>
       </li>
     </ul>
   );
